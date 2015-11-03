4125126c53939b18237c43b3c20e24dd731f5a39
haven <baoyx007@gmail.com>
2015-11-04 00:06:39 +0800

    add .gitignore and finish tcp/udp

---
+++ gui/test_tk.py
@@ -0,0 +1,20 @@
+__author__ = 'haven'
+from tkinter import *
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLable = Label(self, text='hello world')
+        self.helloLable.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
+app.master.title('hello world')
+app.mainloop()
